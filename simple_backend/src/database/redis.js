7e6f3f2e2f017d441a2598367628eb6df6cae770
ZulfahmiFjr <mccraft874@gmail.com>
2026-04-30 19:16:39 +0700

    update redis sama cors

--- simple_backend/src/database/redis.js
+++ simple_backend/src/database/redis.js
@@ -1,9 +1,6 @@
 const Redis = require("ioredis");
 
-const redis = new Redis({
-    host: "127.0.0.1",
-    port: 6379
-});
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on('connect', () => {
     console.log('Connected to redis server!!');
